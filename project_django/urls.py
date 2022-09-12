93e2964c8978d41f0c680b23c0bafe8ae946ba9e
mtrq <muhammad.tarreq@ui.ac.id>
2022-09-12 23:41:48 +0700

    make it work

--- project_django/urls.py
+++ project_django/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from katalog.views import show_katalog
 
-appname = 'katalog'
+#appname = 'katalog'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
